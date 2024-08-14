bb0e23db04b41f89faf5710891af791cd0704eab
KIMGA000 <wldusrkdud@gmail.com>
2024-08-14 17:16:57 +0900

    [feat]상품 목록 가져오기

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,3 +1,5 @@
+const { getProductList } = require("../service/product.service");
+
 const productController = require("express").Router();
 
 // 상품 조회 api
@@ -5,8 +7,22 @@ const productController = require("express").Router();
 // 2) 필터(조건)를 적용한다.
 // 3) 페이지네이션 적용(끊어서 보내주는 역할..?)
 
-productController.get("/", (req, res) => {
-  return res.json({ result: true, data: [] });
+productController.get("/", async (req, res) => {
+  // 상품 전체 조회
+  // 가져온 데이터를 res.json({})에 실어서 클라이언트로 보내준다.
+  try {
+    const productList = await getProductList();
+    return res.status(200).json({
+      result: true,
+      data: productList,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      result: false,
+      message: "(!)상품 목록 가져오기 실패",
+    });
+  }
 });
 
 // 바깥에서 참조할 수 있게끔 해줌
