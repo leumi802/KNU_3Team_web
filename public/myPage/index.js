fac788823bff565293364d03cab900cce69a2031
jsb4662 <jsb4662@gmail.com>
2024-08-14 13:11:04 +0900

    주석수정

--- public/myPage/index.js
+++ public/myPage/index.js
@@ -1,5 +1,5 @@
 // const response = ("3)으로 부터 return 받은 값 저장");
-// 새로운 페이지 시작 시 계속 이 함수 사용..?
+// 새로운 페이지 시작 시 사용
 function responseVerification(response) {
     if (!response.isVerify) {
         console.log("토큰이 유효하지 않습니다. 로그인 페이지로 이동합니다.");
