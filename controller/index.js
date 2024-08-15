c893926b32a1f5b43750462b4f11169bfa4d991e
jsb4662 <jsb4662@gmail.com>
2024-08-16 00:19:11 +0900

    z

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,10 @@
 const apiController = require("express").Router();
 const productController = require("./product.controller");
 const userController = require("./user.controller");
-
+const orderController = require("./order.controller");
 apiController.use("/user", userController);
 apiController.use("/product", productController);
+apiController.use("/order", orderController);
+
 
 module.exports = apiController;
