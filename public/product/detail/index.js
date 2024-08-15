98e16fe583c58e6584ba91394fb3b64ee7bbeff0
jsb4662 <jsb4662@gmail.com>
2024-08-15 14:54:32 +0900

    [feat]상세페이지 기능 구현

--- public/product/detail/index.js
+++ public/product/detail/index.js
@@ -41,7 +41,7 @@ function renderProductDetail(product) {
     const productListWrapper = document.getElementById("product-detail");
     const item = document.createElement("div");
     item.innerHTML = `
-        < div > ${product.title}</div >
+        <div> ${product.title}</div>
             <div>가격: ${product.price}원</div>
             <div>[상세설명] ${product.description}</div>
             <div>
@@ -49,6 +49,65 @@ function renderProductDetail(product) {
             </div>
             <div>재고수량: ${product.stock}(개)</div>
     `;
+
+    // 수량 입력 필드 추가 
+    // input은 1 ~ 재고까지밖에 수정하지 못함
+    const quantityInput = document.createElement('input');
+    quantityInput.type = 'number';
+    quantityInput.min = 1; // 최소 수량 1
+    quantityInput.max = product.stock; // 최대 수량은 재고 수량
+    quantityInput.value = 1; // 기본값 1
+    item.appendChild(quantityInput);
+
+
+    const addCartButton = document.createElement('button');
+    addCartButton.innerHTML = '장바구니';
+    addCartButton.addEventListener("click", () => {
+        const quantity = parseInt(quantityInput.value); // 입력된 수량 가져오기
+        if (quantity > product.stock) {
+            alert("재고 수량보다 많은 수량을 선택할 수 없습니다.");
+            return;
+        } else if (quantity < 0) {
+            alert("1개 미만의 재품을 담을 수 없습니다.");
+        }
+        alert("장바구니에 담겼습니다.");
+
+
+        // localStorage에 저장할 객체 생성
+        const cartItem = {
+            productId: product.productId,
+            quantity: quantity, // 재고 수량
+        };
+
+        // 기존 장바구니 항목 가져오기, 장바구니가 없으면 빈 배열로 초기화
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        if (!cart) {
+            cart = [];
+        }
+
+        // 장바구니에 상품 추가 또는 수량 업데이트
+        const existingItemIndex = cart.findIndex(item => item.productId === product.productId);
+        if (existingItemIndex > -1) {
+            // 이미 장바구니에 있는 상품의 경우 수량 업데이트
+            cart[existingItemIndex].quantity = quantity;
+        } else {
+            // 장바구니에 없는 상품인 경우 새로 추가
+            cart.push(cartItem);
+        }
+
+        // 업데이트된 장바구니를 localStorage에 저장
+        localStorage.setItem('cart', JSON.stringify(cart));
+    });
+
+    const payButton = document.createElement('button');
+    payButton.innerHTML = '결제하기';
+    payButton.addEventListener("click", async () => {
+        window.location.href = "/cart";
+    });
+
+    item.append(addCartButton);
+    item.append(payButton);
+
     productListWrapper.append(item);
     console.log("상품을 정상적으로 업로딩했습니다.");
 }
