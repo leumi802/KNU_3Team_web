ddb4d2d453c30f2b6927675c1d590dbffb2abdcc
KIMGA000 <wldusrkdud@gmail.com>
2024-08-15 16:44:41 +0900

    [feat]장바구니 페이지 수정

--- public/cart/index.js
+++ public/cart/index.js
@@ -22,6 +22,13 @@ const fetchProductList = async () => {
 
 // 상품 상세 페이지 로드 시
 window.addEventListener("load", async () => {
+  const token = localStorage.getItem("token");
+  // token이 가져와졌는지 체크
+  if (token === null) {
+    // 오류 뜰 곳
+    alert("(!)토큰이 존재하지 않음.");
+    window.location.href = "http://localhost:8000/signin";
+  }
   try {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
     const productList = await fetchProductList(); // 상품 목록을 먼저 가져옵니다
